3a77edea7fa81e9d6b6a9368bdc00c1021cfec54
Amaar Quadri <aq@trexorobotics.com>
2021-10-15 17:30:42 -0400

    added U Perms

--- include/PLL.h
+++ include/PLL.h
@@ -2,5 +2,7 @@
 
 #include "Algorithm.h"
 
-const static Algorithm T_PERM = Algorithm::parse("RUR'U'R'FR2U'R'U'RUR'F'");
-const static Algorithm Y_PERM = Algorithm::parse("FRU'R'U'RUR'F'RUR'U'R'FRF'");
+const static Algorithm T_PERM = Algorithm::parse("RUR'U'R'FR2U'R'U'RUR'F'"); // NOLINT(cert-err58-cpp)
+const static Algorithm Y_PERM = Algorithm::parse("FRU'R'U'RUR'F'RUR'U'R'FRF'"); // NOLINT(cert-err58-cpp)
+const static Algorithm Ua_PERM = Algorithm::parse("R2U'R'U'RURURU'R"); // NOLINT(cert-err58-cpp)
+const static Algorithm Ub_PERM = Ua_PERM.inv(); // NOLINT(cert-err58-cpp)
