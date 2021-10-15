351489d0a9d6ac7646f72e2b8fc4502ae700be7f
Amaar Quadri <aq@trexorobotics.com>
2021-10-15 13:02:09 -0400

    use namespace resolution operator to fix build error

--- src/Turn.cpp
+++ src/Turn.cpp
@@ -1,5 +1,7 @@
 #include "Turn.h"
 
 std::string Turn::toStr() const {
-    return toStr(face) + toStr(rotationAmount);
+    // This syntax tells C++ to use the functions in the global namespace instead of recursion
+    // Not sure why this is needed since the functions have different signatures
+    return ::toStr(face) + ::toStr(rotationAmount);
 }
