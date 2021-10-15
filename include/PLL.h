6f8ba15c1efee3970e3966769c7a21374b38659c
Amaar Quadri <aq@trexorobotics.com>
2021-10-15 17:09:01 -0400

    added header file with PLL algs

---
+++ include/PLL.h
@@ -0,0 +1,6 @@
+#pragma once
+
+#include "Algorithm.h"
+
+const static Algorithm T_PERM = Algorithm::parse("RUR'U'R'FR2U'R'U'RUR'F'");
+const static Algorithm Y_PERM = Algorithm::parse("FRU'R'U'RUR'F'RUR'U'R'FRF'");
