68be3253a3d1817b37917c5238cbcebf97b7f277
Amaar Quadri <amaarquadri@gmail.com>
2021-10-31 02:11:11 -0400

    add spaces in PPLs

--- include/PLL.h
+++ include/PLL.h
@@ -2,7 +2,7 @@
 
 #include "Algorithm.h"
 
-const static Algorithm T_PERM = Algorithm::parse("RUR'U'R'FR2U'R'U'RUR'F'"); // NOLINT(cert-err58-cpp)
-const static Algorithm Y_PERM = Algorithm::parse("FRU'R'U'RUR'F'RUR'U'R'FRF'"); // NOLINT(cert-err58-cpp)
-const static Algorithm Ua_PERM = Algorithm::parse("R2U'R'U'RURURU'R"); // NOLINT(cert-err58-cpp)
+const static Algorithm T_PERM = Algorithm::parse("R U R' U' R' F R2 U' R' U' R U R' F'"); // NOLINT(cert-err58-cpp)
+const static Algorithm Y_PERM = Algorithm::parse("F R U' R' U' R U R' F' R U R' U' R' F R F'"); // NOLINT(cert-err58-cpp)
+const static Algorithm Ua_PERM = Algorithm::parse("R2 U' R' U' R U R U R U' R"); // NOLINT(cert-err58-cpp)
 const static Algorithm Ub_PERM = Ua_PERM.inv(); // NOLINT(cert-err58-cpp)
