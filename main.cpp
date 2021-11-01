a10dbecfa1cf38191901612aacdadb85a6493f57
Amaar Quadri <amaarquadri@gmail.com>
2021-10-31 23:34:33 -0400

    load input from file

--- main.cpp
+++ main.cpp
@@ -1,12 +1,20 @@
-#include <iostream>
-#include "PLL.h"
+#include "Algorithm.h"
 #include "Cube.h"
+#include <iostream>
+#include <fstream>
+#include <sstream>
+
+Algorithm loadInput() {
+    std::ifstream file("input.txt");
+    std::stringstream buffer;
+    buffer << file.rdbuf();
+    return Algorithm::parse(buffer.str());
+}
 
 int main() {
+    Algorithm moves = loadInput();
     Cube cube{};
-    cube.apply(Ua_PERM);
-    std::cout << cube.isSolved() << std::endl;
-    cube.apply(Ub_PERM);
+    cube.apply(moves);
     std::cout << cube.isSolved() << std::endl;
     return 0;
 }
