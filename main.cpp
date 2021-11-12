481194d76be59f19c1540f76d0fe96b508a255ca
Amaar Quadri <amaarquadri@gmail.com>
2021-11-11 22:42:21 -0500

    clean up

--- main.cpp
+++ main.cpp
@@ -17,10 +17,12 @@ int main() {
     auto [scramble, solve] = loadInput();
     Reconstruction reconstruction = parseSolveAttempt(solve);
     std::cout << reconstruction.toStr() << std::endl;
-//    std::vector<Reconstruction> possible_reconstructions = getPossibleReconstructions(scramble);
-//    std::vector<unsigned int> edit_distances = sortBestReconstructions(reconstruction, possible_reconstructions);
-//    for (size_t i = 0; i < possible_reconstructions.size(); i++) {
-//        std::cout << "Distance: " << edit_distances[i] << ", " << possible_reconstructions[i].toStr() << std::endl;
-//    }
+    std::vector<Reconstruction> possible_reconstructions = getPossibleReconstructions(scramble);
+    std::vector<unsigned int> edit_distances = sortBestReconstructions(reconstruction, possible_reconstructions);
+
+    std::cout << "Possible Reconstructions:" << std::endl;
+    for (size_t i = 0; i < possible_reconstructions.size(); i++) {
+        std::cout << "Edit Distance: " << edit_distances[i] << ", " << possible_reconstructions[i].toStr() << std::endl;
+    }
     return 0;
 }
