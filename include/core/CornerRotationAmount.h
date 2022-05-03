9a53035ff43467d7b9efc308e59f4ed6952882b6
Amaar Quadri <amaarquadri@gmail.com>
2022-05-02 20:19:25 -0400

    created CornerRotationAmount

---
+++ include/core/CornerRotationAmount.h
@@ -0,0 +1,11 @@
+#pragma once
+
+#include <cstdint>
+
+enum CornerRotationAmount : uint8_t {
+    NONE, CLOCKWISE, COUNTERCLOCKWISE
+};
+
+CornerRotationAmount inv(const CornerRotationAmount &rotationAmount);
+
+CornerRotationAmount operator+(const CornerRotationAmount &first, const CornerRotationAmount &second);
---
+++ src/core/CornerRotationAmount.cpp
@@ -0,0 +1,20 @@
+#include "CornerRotationAmount.h"
+#include <stdexcept>
+
+CornerRotationAmount inv(const CornerRotationAmount &rotationAmount) {
+    switch (rotationAmount) {
+        case NONE:
+            return NONE;
+        case CLOCKWISE:
+            return COUNTERCLOCKWISE;
+        case COUNTERCLOCKWISE:
+            return CLOCKWISE;
+        default:
+            throw std::logic_error("Unknown enum value!");
+    }
+}
+
+CornerRotationAmount operator+(const CornerRotationAmount &first, const CornerRotationAmount &second) {
+    uint8_t clockwiseTurns = (static_cast<uint8_t>(first) + static_cast<uint8_t>(second)) % 3;
+    return static_cast<CornerRotationAmount>(clockwiseTurns);
+}
