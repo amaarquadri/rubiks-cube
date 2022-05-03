eb6265b003833d458d3f5aa766ffe2ca8453edf3
Amaar Quadri <amaarquadri@gmail.com>
2022-05-02 20:41:40 -0400

    code formatting for main and PLL

--- include/PLL.h
+++ include/PLL.h
@@ -2,30 +2,44 @@
 
 #include "Algorithm.h"
 
-const Algorithm T_PERM = Algorithm::parse("R U R' U' R' F R2 U' R' U' R U R' F'"); // NOLINT(cert-err58-cpp)
-const Algorithm Y_PERM = Algorithm::parse("F R U' R' U' R U R' F' R U R' U' R' F R F'"); // NOLINT(cert-err58-cpp)
-const Algorithm Ua_PERM = Algorithm::parse("R2 U' R' U' R U R U R U' R"); // NOLINT(cert-err58-cpp)
-const Algorithm Ub_PERM = Ua_PERM.inv(); // NOLINT(cert-err58-cpp)
+const Algorithm T_PERM = Algorithm::parse(
+    "R U R' U' R' F R2 U' R' U' R U R' F'");  // NOLINT(cert-err58-cpp)
+const Algorithm Y_PERM = Algorithm::parse(
+    "F R U' R' U' R U R' F' R U R' U' R' F R F'");  // NOLINT(cert-err58-cpp)
+const Algorithm Ua_PERM =
+    Algorithm::parse("R2 U' R' U' R U R U R U' R");  // NOLINT(cert-err58-cpp)
+const Algorithm Ub_PERM = Ua_PERM.inv();             // NOLINT(cert-err58-cpp)
 
 // TODO: check which one is which
-const Algorithm Ja_PERM = Algorithm::parse("R U R' F' R U R' U' R' F R2 U' R' U'"); // NOLINT(cert-err58-cpp)
-const Algorithm Jb_PERM = Algorithm::parse("L' U2 L U L' U2 R U' L U R'"); // NOLINT(cert-err58-cpp)
+const Algorithm Ja_PERM = Algorithm::parse(
+    "R U R' F' R U R' U' R' F R2 U' R' U'");  // NOLINT(cert-err58-cpp)
+const Algorithm Jb_PERM =
+    Algorithm::parse("L' U2 L U L' U2 R U' L U R'");  // NOLINT(cert-err58-cpp)
 
 // TODO: check which one is which
-const Algorithm Aa_PERM = Algorithm::parse("R' F R' B2 R F' R' B2 R2"); // NOLINT(cert-err58-cpp)
-const Algorithm Ab_PERM = Aa_PERM.inv(); // NOLINT(cert-err58-cpp)
-const Algorithm F_PERM = Algorithm::parse("R' U' F' R U R' U' R' F R2 U' R' U' R U R' U R"); // NOLINT(cert-err58-cpp)
-const Algorithm E_PERM = Algorithm::commutator("R B' R'", "D") + Algorithm::commutator("R B R'", "D"); // NOLINT(cert-err58-cpp)
-const Algorithm H_PERM = Algorithm::parse("M2 U' M2 U2 M2 U' M2"); // NOLINT(cert-err58-cpp)
-const Algorithm Z_PERM = Algorithm::parse("M2 U' M2 U' M' U2 M2 U2 M' U2"); // NOLINT(cert-err58-cpp)
-const Algorithm V_PERM = Algorithm::parse("R' U R' U' X "); // NOLINT(cert-err58-cpp)
+const Algorithm Aa_PERM =
+    Algorithm::parse("R' F R' B2 R F' R' B2 R2");  // NOLINT(cert-err58-cpp)
+const Algorithm Ab_PERM = Aa_PERM.inv();           // NOLINT(cert-err58-cpp)
+const Algorithm F_PERM = Algorithm::parse(
+    "R' U' F' R U R' U' R' F R2 U' R' U' R U R' U R");  // NOLINT(cert-err58-cpp)
+const Algorithm E_PERM =
+    Algorithm::commutator("R B' R'", "D") +
+    Algorithm::commutator("R B R'", "D");  // NOLINT(cert-err58-cpp)
+const Algorithm H_PERM =
+    Algorithm::parse("M2 U' M2 U2 M2 U' M2");  // NOLINT(cert-err58-cpp)
+const Algorithm Z_PERM = Algorithm::parse(
+    "M2 U' M2 U' M' U2 M2 U2 M' U2");  // NOLINT(cert-err58-cpp)
+const Algorithm V_PERM =
+    Algorithm::parse("R' U R' U' X ");  // NOLINT(cert-err58-cpp)
 
 // TODO: check which one is which
-const Algorithm Ga_PERM = Algorithm::parse(""); // NOLINT(cert-err58-cpp)
-const Algorithm Gb_PERM = Algorithm::parse(""); // NOLINT(cert-err58-cpp)
-const Algorithm Gc_PERM = Algorithm::parse(""); // NOLINT(cert-err58-cpp)
-const Algorithm Gd_PERM = Algorithm::parse(""); // NOLINT(cert-err58-cpp)
+const Algorithm Ga_PERM = Algorithm::parse("");  // NOLINT(cert-err58-cpp)
+const Algorithm Gb_PERM = Algorithm::parse("");  // NOLINT(cert-err58-cpp)
+const Algorithm Gc_PERM = Algorithm::parse("");  // NOLINT(cert-err58-cpp)
+const Algorithm Gd_PERM = Algorithm::parse("");  // NOLINT(cert-err58-cpp)
 
 // TODO: check which one is which
-const Algorithm Na_PERM = Ja_PERM.withSetup("R U R' U"); // NOLINT(cert-err58-cpp)
-const Algorithm Nb_PERM = Jb_PERM.withSetup("L' U' L U'"); // NOLINT(cert-err58-cpp)
+const Algorithm Na_PERM =
+    Ja_PERM.withSetup("R U R' U");  // NOLINT(cert-err58-cpp)
+const Algorithm Nb_PERM =
+    Jb_PERM.withSetup("L' U' L U'");  // NOLINT(cert-err58-cpp)
--- main.cpp
+++ main.cpp
@@ -1,28 +1,31 @@
 #include "Algorithm.h"
 #include "Blindsolving.h"
-#include <iostream>
 #include <fstream>
+#include <iostream>
 #include <sstream>
 
 using namespace blindsolving;
 
 std::pair<Algorithm, Algorithm> loadInput() {
-    std::ifstream file("tests/blindsolve3.txt");
-    std::stringstream buffer;
-    buffer << file.rdbuf();
-    return Algorithm::parseScrambleSolve(buffer.str());
+  std::ifstream file("tests/blindsolve3.txt");
+  std::stringstream buffer;
+  buffer << file.rdbuf();
+  return Algorithm::parseScrambleSolve(buffer.str());
 }
 
 int main() {
-    auto [scramble, solve] = loadInput();
-    Reconstruction reconstruction = parseSolveAttempt(solve);
-    std::cout << reconstruction.toStr() << std::endl;
-    std::vector<Reconstruction> possible_reconstructions = getPossibleReconstructions(scramble);
-    std::vector<unsigned int> edit_distances = sortBestReconstructions(reconstruction, possible_reconstructions);
+  auto [scramble, solve] = loadInput();
+  Reconstruction reconstruction = parseSolveAttempt(solve);
+  std::cout << reconstruction.toStr() << std::endl;
+  std::vector<Reconstruction> possible_reconstructions =
+      getPossibleReconstructions(scramble);
+  std::vector<unsigned int> edit_distances =
+      sortBestReconstructions(reconstruction, possible_reconstructions);
 
-    std::cout << "Possible Reconstructions:" << std::endl;
-    for (size_t i = 0; i < possible_reconstructions.size(); i++) {
-        std::cout << "Edit Distance: " << edit_distances[i] << ", " << possible_reconstructions[i].toStr() << std::endl;
-    }
-    return 0;
+  std::cout << "Possible Reconstructions:" << std::endl;
+  for (size_t i = 0; i < possible_reconstructions.size(); i++) {
+    std::cout << "Edit Distance: " << edit_distances[i] << ", "
+              << possible_reconstructions[i].toStr() << std::endl;
+  }
+  return 0;
 }
