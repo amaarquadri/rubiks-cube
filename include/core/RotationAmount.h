db942a474e380885555e71303cb544bb3d248894
Amaar Quadri <amaarquadri@gmail.com>
2022-05-23 00:07:34 +0200

    cleanup, add TODO

--- include/core/RotationAmount.h
+++ include/core/RotationAmount.h
@@ -4,7 +4,13 @@
 #include <string>
 #include <utility>
 
-enum class RotationAmount : uint8_t { NONE, CLOCKWISE, HALF_TURN, COUNTERCLOCKWISE };
+enum class RotationAmount : uint8_t {
+  // TODO: fix naming convention
+  NONE,
+  CLOCKWISE,
+  HALF_TURN,
+  COUNTERCLOCKWISE
+};
 
 RotationAmount inv(const RotationAmount& rotationAmount);
 
