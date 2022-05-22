d2bf6521e0731ad7ad0dd7be3ef4de2b248cecdc
Amaar Quadri <amaarquadri@gmail.com>
2022-05-23 01:46:41 +0200

    Add Levenshtein edit distance function to Utils.h

--- include/utility/Utils.h
+++ include/utility/Utils.h
@@ -18,4 +18,37 @@ std::vector<T> flatten(const std::vector<std::vector<T>>& deep_vec) {
     for (const auto& v : vec) flattened.push_back(v);
   return flattened;
 }
+
+/***
+ * Computes the Levenshtein edit distance
+ */
+template <typename T>
+unsigned int levEditDistance(const std::vector<T>& first,
+                             const std::vector<T>& second) {
+  const size_t N = first.size();
+  const size_t M = second.size();
+
+  // ensure that M <= N, to reduce memory usage
+  if (N < M) return levEditDistance(second, first);
+
+  std::vector<unsigned int> current_row(M);
+  for (size_t m = 0; m < M; m++) current_row[m] = m;
+
+  for (size_t n = 1; n < N; n++) {
+    unsigned int last_row_last_column = current_row[0];
+    current_row[0] = n;
+    for (size_t m = 1; m < M; m++) {
+      if (first[n] != second[m]) {
+        // store the desired value of current_row[m] in last_row_last_column
+        if (current_row[m - 1] < last_row_last_column)
+          last_row_last_column = current_row[m - 1];
+        if (current_row[m] < last_row_last_column)
+          last_row_last_column = current_row[m];
+        last_row_last_column++;
+      }
+      std::swap(last_row_last_column, current_row[m]);
+    }
+  }
+  return current_row[M - 1];
+}
 }  // namespace utility
