894a343c64982457203ae94bfcfa11e24abcc200
Amaar Quadri <amaarquadri@gmail.com>
2022-05-23 03:42:57 +0200

    add tests

---
+++ tests/blindsolving/TestCornerCycleSequenceIterator.h
@@ -0,0 +1,31 @@
+#include "Blindsolving.h"
+#include "CornerCycleSequenceIterator.h"
+#include "CornerRotationAmount.h"
+#include "Cube.h"
+#include "Utils.h"
+#include <iostream>
+#include <vector>
+
+static Cube applyCornerAlgs(const std::vector<char>& chars) {
+  Cube cube{};
+  for (const char& c : chars)
+    cube.cycleCorners<2>({blindsolving::CORNER_BUFFER,
+                          blindsolving::REVERSE_CORNER_LETTERING.at(c)});
+  return cube;
+}
+
+void testCornerCycleSequenceIterator() {
+  std::vector<std::vector<char>> cycles;
+  cycles.push_back({'B', 'C'});
+  cycles.push_back({'D', 'G', 'U'});
+  cycles.push_back({'Z', 'K'});
+  std::vector<CornerRotationAmount> rotation_amounts{
+      CornerRotationAmount::NONE, CornerRotationAmount::CLOCKWISE,
+      CornerRotationAmount::COUNTERCLOCKWISE};
+  blindsolving::CornerCycleSequenceIterator it{cycles, rotation_amounts};
+  const Cube cube = applyCornerAlgs(*it);
+  while (++it)
+    if (cube != applyCornerAlgs(*it)) throw std::logic_error("Mismatch!");
+
+  std::cout << "Passed all tests for CornerCycleSequenceIterator!\n";
+}
---
+++ tests/blindsolving/TestEdgeCycleSequenceIterator.h
@@ -0,0 +1,28 @@
+#include "Blindsolving.h"
+#include "Cube.h"
+#include "EdgeCycleSequenceIterator.h"
+#include "Utils.h"
+#include <iostream>
+#include <vector>
+
+static Cube applyEdgeAlgs(const std::vector<char>& chars) {
+  Cube cube{};
+  for (const char& c : chars)
+    cube.cycleEdges<2>({blindsolving::EDGE_BUFFER,
+                        blindsolving::REVERSE_EDGE_LETTERING.at(c)});
+  return cube;
+}
+
+static void testEdgeCycleSequenceIterator() {
+  std::vector<std::vector<char>> cycles;
+  cycles.push_back({'A', 'B', 'C'});
+  cycles.push_back({'D', 'F'});
+  cycles.push_back({'H', 'J', 'K', 'N'});
+  std::vector<bool> are_even{false, true, false};
+  blindsolving::EdgeCycleSequenceIterator it{cycles, are_even};
+  const Cube cube = applyEdgeAlgs(*it);
+  while (++it)
+    if (cube != applyEdgeAlgs(*it)) throw std::logic_error("Mismatch!");
+
+  std::cout << "Passed all tests for EdgeCycleSequenceIterator!\n";
+}
