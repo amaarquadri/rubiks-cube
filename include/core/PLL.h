8e1360a534cce3539ae9749b9c59e2819294b630
Amaar Quadri <amaarquadri@gmail.com>
2022-05-24 23:30:35 +0200

    remove unnecessary lint suppressors

--- include/core/PLL.h
+++ include/core/PLL.h
@@ -2,31 +2,25 @@
 
 #include "Algorithm.h"
 
-const Algorithm T_PERM = Algorithm::parse(
-    "R U R' U' R' F R2 U' R' U' R U R' F'");  // NOLINT(cert-err58-cpp)
-const Algorithm Y_PERM = Algorithm::parse(
-    "F R U' R' U' R U R' F' R U R' U' R' F R F'");  // NOLINT(cert-err58-cpp)
-const Algorithm Ua_PERM =
-    Algorithm::parse("R2 U' R' U' R U R U R U' R");  // NOLINT(cert-err58-cpp)
-const Algorithm Ub_PERM = Ua_PERM.inv();             // NOLINT(cert-err58-cpp)
+const Algorithm T_PERM =
+    Algorithm::parse("R U R' U' R' F R2 U' R' U' R U R' F'");
+const Algorithm Y_PERM =
+    Algorithm::parse("F R U' R' U' R U R' F' R U R' U' R' F R F'");
+const Algorithm Ua_PERM = Algorithm::parse("R2 U' R' U' R U R U R U' R");
+const Algorithm Ub_PERM = Ua_PERM.inv();
 
-const Algorithm Ja_PERM =
-    Algorithm::parse("L' U2 L U L' U2 R U' L U R'");  // NOLINT(cert-err58-cpp)
-const Algorithm Jb_PERM = Algorithm::parse(
-    "R U R' F' R U R' U' R' F R2 U' R' U'");  // NOLINT(cert-err58-cpp)
+const Algorithm Ja_PERM = Algorithm::parse("L' U2 L U L' U2 R U' L U R'");
+const Algorithm Jb_PERM =
+    Algorithm::parse("R U R' F' R U R' U' R' F R2 U' R' U'");
 
-const Algorithm Aa_PERM =
-    Algorithm::parse("R' F R' B2 R F' R' B2 R2");  // NOLINT(cert-err58-cpp)
-const Algorithm Ab_PERM = Aa_PERM.inv();           // NOLINT(cert-err58-cpp)
-const Algorithm F_PERM = Algorithm::parse(
-    "R' U' F' R U R' U' R' F R2 U' R' U' R U R' U R");  // NOLINT(cert-err58-cpp)
-const Algorithm E_PERM =
-    Algorithm::commutator("R B' R'", "F") +
-    Algorithm::commutator("R B R'", "F");  // NOLINT(cert-err58-cpp)
-const Algorithm H_PERM =
-    Algorithm::parse("M2 U' M2 U2 M2 U' M2");  // NOLINT(cert-err58-cpp)
-const Algorithm Z_PERM = Algorithm::parse(
-    "M2 U' M2 U' M' U2 M2 U2 M' U2");  // NOLINT(cert-err58-cpp)
+const Algorithm Aa_PERM = Algorithm::parse("R' F R' B2 R F' R' B2 R2");
+const Algorithm Ab_PERM = Aa_PERM.inv();
+const Algorithm F_PERM =
+    Algorithm::parse("R' U' F' R U R' U' R' F R2 U' R' U' R U R' U R");
+const Algorithm E_PERM = Algorithm::commutator("R B' R'", "F") +
+                         Algorithm::commutator("R B R'", "F");
+const Algorithm H_PERM = Algorithm::parse("M2 U' M2 U2 M2 U' M2");
+const Algorithm Z_PERM = Algorithm::parse("M2 U' M2 U' M' U2 M2 U2 M' U2");
 const Algorithm V_PERM =
     Algorithm::parse("R' U R' U' Y R' F' R2 U' R' U R' F R F");
 
@@ -39,8 +33,5 @@ const Algorithm Gc_PERM =
 const Algorithm Gd_PERM =
     Algorithm::parse("R U R' Y' R2 D' Y' R U' R' U R' D Y R2");
 
-// TODO: check which one is which
-const Algorithm Na_PERM =
-    Jb_PERM.withSetup("R U R' U");  // NOLINT(cert-err58-cpp)
-const Algorithm Nb_PERM =
-    Ja_PERM.withSetup("L' U' L U'");  // NOLINT(cert-err58-cpp)
+const Algorithm Na_PERM = Jb_PERM.withSetup("R U R' U");
+const Algorithm Nb_PERM = Ja_PERM.withSetup("L' U' L U'");
