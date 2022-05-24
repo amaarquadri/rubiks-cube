4a5a71ae2e1f3a9811987d280b2aaaee666b72b5
Amaar Quadri <amaarquadri@gmail.com>
2022-05-24 16:44:18 +0200

    allow hashing BlindsolvingMove and BlindsolvingReconstruction

--- include/blindsolving/BlindsolvingMove.h
+++ include/blindsolving/BlindsolvingMove.h
@@ -29,3 +29,13 @@ struct BlindsolvingMove {
   constexpr ~BlindsolvingMove(){};
 };
 }  // namespace blindsolving
+
+namespace std {
+template <>
+struct hash<blindsolving::BlindsolvingMove> {
+  size_t operator()(const blindsolving::BlindsolvingMove& move) const {
+    if (move.is_parity) return 1229;  // arbitrary large prime number
+    return 31 * move.alg + (move.is_edge ? 1231 : 1237);
+  }
+};
+}  // namespace std
--- include/blindsolving/BlindsolvingReconstruction.h
+++ include/blindsolving/BlindsolvingReconstruction.h
@@ -13,3 +13,16 @@ class BlindsolvingReconstruction : public std::vector<BlindsolvingMove> {
   std::string toStr() const;
 };
 }  // namespace blindsolving
+
+namespace std {
+template <>
+struct hash<blindsolving::BlindsolvingReconstruction> {
+  size_t operator()(
+      const blindsolving::BlindsolvingReconstruction& recon) const {
+    static constexpr hash<blindsolving::BlindsolvingMove> hasher{};
+    size_t val = 1249;  // arbitrary large prime
+    for (const auto& move : recon) val = 31 * val + hasher(move);
+    return val;
+  }
+};
+}  // namespace std
