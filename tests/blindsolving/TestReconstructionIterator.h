d56d54ea32f6d8983e4f5c54cbb82aaacbb087aa
Amaar Quadri <amaarquadri@gmail.com>
2022-05-24 17:13:41 +0200

    avoid repeatedly scrambling the cube, ensure all reconstructions are unique

--- tests/blindsolving/TestReconstructionIterator.h
+++ tests/blindsolving/TestReconstructionIterator.h
@@ -6,18 +6,30 @@
 #include "ReconstructionIterator.h"
 #include <iostream>
 #include <stdexcept>
+#include <unordered_set>
 
 void testReconstructionIterator(const Algorithm& scramble) {
-  blindsolving::ReconstructionIterator it =
-      blindsolving::getReconstructionIterator(scramble);
-  std::cout << "Testing " << it.getPeriod() << " reconstructions...\n";
-  do {
+  const Cube scrambled_cube = [&]() {
     Cube cube{};
     cube.apply(scramble);
-    (*it).applyTo(cube);
+    return cube;
+  }();
+
+  blindsolving::ReconstructionIterator it =
+      blindsolving::getReconstructionIterator(scramble);
+  std::unordered_set<blindsolving::BlindsolvingReconstruction> recons{};
+  std::cout << "Testing " << it.getPeriod() << " reconstructions...\n";
+  do {
+    const blindsolving::BlindsolvingReconstruction recon = *it;
+    if (!recons.insert(recon).second)
+      throw std::logic_error("Duplicate reconstruction!");
+    Cube cube = scrambled_cube;
+    recon.applyTo(cube);
     if (!cube.isSolved())
       throw std::logic_error(
           "BlindsolvingReconstruction did not solve the scramble!");
   } while (++it);
+  if (recons.size() != it.getPeriod())
+    throw std::logic_error("Incorrect number of unique reconstructions!");
   std::cout << "Passed all tests for ReconstructionIterator!\n";
 }
