699ad85d45c7be11cbfa4a71e552042a195dc96c
Amaar Quadri <amaarquadri@gmail.com>
2022-05-25 02:40:26 +0200

    delete ::inv(CornerRotationAmount) since operator- can be used instead

--- include/core/CornerRotationAmount.h
+++ include/core/CornerRotationAmount.h
@@ -4,8 +4,6 @@
 
 enum class CornerRotationAmount : uint8_t { NONE, CLOCKWISE, COUNTERCLOCKWISE };
 
-CornerRotationAmount inv(const CornerRotationAmount& rotationAmount);
-
 CornerRotationAmount operator+(const CornerRotationAmount& first,
                                const CornerRotationAmount& second);
 
--- src/core/CornerRotationAmount.cpp
+++ src/core/CornerRotationAmount.cpp
@@ -1,18 +1,4 @@
 #include "CornerRotationAmount.h"
-#include <stdexcept>
-
-CornerRotationAmount inv(const CornerRotationAmount& rotationAmount) {
-  switch (rotationAmount) {
-    case CornerRotationAmount::NONE:
-      return CornerRotationAmount::NONE;
-    case CornerRotationAmount::CLOCKWISE:
-      return CornerRotationAmount::COUNTERCLOCKWISE;
-    case CornerRotationAmount::COUNTERCLOCKWISE:
-      return CornerRotationAmount::CLOCKWISE;
-    default:
-      throw std::logic_error("Unknown enum value!");
-  }
-}
 
 CornerRotationAmount operator+(const CornerRotationAmount& first,
                                const CornerRotationAmount& second) {
