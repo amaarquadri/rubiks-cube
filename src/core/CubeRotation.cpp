eab6df76bb63d9849d42815d0cfcf13a11fde8aa
Amaar Quadri <amaarquadri@gmail.com>
2022-05-25 04:02:19 +0200

    add missing consts

--- src/core/CubeRotation.cpp
+++ src/core/CubeRotation.cpp
@@ -9,11 +9,12 @@ std::string CubeRotation::toStr() const {
 }
 
 std::pair<size_t, CubeRotation> CubeRotation::parse(const std::string& str) {
-  auto [consumed_for_rotation_axis, rotationAxis] = parseRotationAxis(str);
+  const auto [consumed_for_rotation_axis, rotationAxis] =
+      parseRotationAxis(str);
   if (consumed_for_rotation_axis == 0) return {0, {}};  // not possible to parse
-  std::string remaining = str.substr(consumed_for_rotation_axis,
-                                     str.size() - consumed_for_rotation_axis);
-  auto [consumed_for_rotation_amount, rotation_amount] =
+  const std::string remaining = str.substr(
+      consumed_for_rotation_axis, str.size() - consumed_for_rotation_axis);
+  const auto [consumed_for_rotation_amount, rotation_amount] =
       parseRotationAmount(remaining);
   return {consumed_for_rotation_axis + consumed_for_rotation_amount,
           {rotationAxis, rotation_amount}};
