53f50266ae0eaa91d2b8ea3c57394da1386a0549
Amaar Quadri <amaarquadri@gmail.com>
2022-06-04 15:16:18 +0200

    make factorial constexpr

--- include/utility/MathUtils.h
+++ include/utility/MathUtils.h
@@ -2,7 +2,7 @@
 
 namespace utility {
 template <typename T>
-T factorial(const T& n) {
+constexpr T factorial(const T& n) {
   T result = static_cast<T>(1);
   // decrement instead of incrementing in the loop to avoid an infinite loop in
   // the case where n is the maximum value
