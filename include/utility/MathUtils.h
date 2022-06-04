343001eed0f8a3b06c3bb5ea5f87aed53810c138
Amaar Quadri <amaarquadri@gmail.com>
2022-06-04 15:17:50 +0200

    implement constexpr pow and nChooseK with test cases

--- include/utility/MathUtils.h
+++ include/utility/MathUtils.h
@@ -1,5 +1,8 @@
 #pragma once
 
+#include <cstddef>
+#include <type_traits>
+
 namespace utility {
 template <typename T>
 constexpr T factorial(const T& n) {
@@ -9,4 +12,28 @@ constexpr T factorial(const T& n) {
   for (T val = n; val >= static_cast<T>(2); --val) result *= val;
   return result;
 }
+
+template <typename T1, typename T2>
+constexpr auto pow(const T1& base, const T2& exponent) {
+  using R = std::common_type_t<T1, T2>;
+  R result = static_cast<R>(1);
+  for (size_t i = 0; i < exponent; ++i) result *= base;
+  return result;
+}
+
+template <typename T1, typename T2>
+constexpr auto nChooseK(const T1& n, const T2& k) {
+  using R = std::common_type_t<T1, T2>;
+  R result = static_cast<R>(1);
+  for (R i = static_cast<R>(0); i < k; ++i) {
+    result *= (n - i);
+    result /= (i + static_cast<R>(1));
+  }
+  return result;
+}
+
+// Test cases
+static_assert(factorial(5) == 120);
+static_assert(pow(3, 7) == 2187);
+static_assert(nChooseK(12, 4) == 495);
 }  // namespace utility
