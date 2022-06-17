921d8a7c4bdbc93fee2fcd5ef63aeea046991741
Amaar Quadri <amaarquadri@gmail.com>
2022-06-17 02:05:47 -0400

    refactor Permutation to subclass std::array

--- include/utility/Permutation.h
+++ include/utility/Permutation.h
@@ -10,32 +10,26 @@
 #include <utility>
 
 template <size_t n>
-class Permutation {
- private:
-  std::array<uint8_t, n> values;
-
-  explicit Permutation(std::array<uint8_t, n> values)
-      : values(std::move(values)) {}
-
+class Permutation : public std::array<uint8_t, n> {
  public:
   static Permutation<n> randomPermutation() {
     if (n < 2) throw std::invalid_argument("Error: n < 2");
-    std::array<uint8_t, n> values;
-    std::iota(values.begin(), values.end(), 0);
-    std::shuffle(values.begin(), values.end(), utility::random_engine);
-    return Permutation{values};
+    Permutation<n> permutation;
+    std::iota(permutation.begin(), permutation.end(), 0);
+    std::shuffle(permutation.begin(), permutation.end(), utility::random_engine);
+    return permutation;
   }
 
   [[nodiscard]] bool isOdd() const {
     std::array<bool, n> visited{};
     bool parity = false;
-    for (size_t starting_index = 0; starting_index < n; starting_index++) {
+    for (size_t starting_index = 0; starting_index < n; ++starting_index) {
       if (!visited[starting_index]) {
         visited[starting_index] = true;
-        uint8_t cycle_index = values[starting_index];
+        uint8_t cycle_index = (*this)[starting_index];
         while (cycle_index != starting_index) {
           visited[cycle_index] = true;
-          cycle_index = values[cycle_index];
+          cycle_index = (*this)[cycle_index];
           parity = !parity;
         }
       }
@@ -43,12 +37,12 @@ class Permutation {
     return parity;
   }
 
-  void flipParity() { std::swap(values[0], values[1]); }
+  void flipParity() { std::swap(this->front(), (*this)[1]); }
 
   template <typename T>
   std::array<T, n> apply(const std::array<T, n>& items) const {
     std::array<T, n> permuted_items;
-    for (size_t i = 0; i < n; i++) permuted_items[i] = items[values[i]];
+    for (size_t i = 0; i < n; ++i) permuted_items[i] = items[(*this)[i]];
     return permuted_items;
   }
 };
