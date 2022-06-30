02bd1979a5bec28b8820821025187372236fa791
Amaar Quadri <amaarquadri@gmail.com>
2022-06-29 19:53:57 -0600

    standardize ++

--- include/utility/Utils.h
+++ include/utility/Utils.h
@@ -50,19 +50,19 @@ size_t levEditDistance(const std::vector<T>& first,
   if (N < M) return levEditDistance(second, first);
 
   std::vector<size_t> current_row(M);
-  for (size_t m = 0; m < M; m++) current_row[m] = m;
+  for (size_t m = 0; m < M; ++m) current_row[m] = m;
 
-  for (size_t n = 1; n < N; n++) {
+  for (size_t n = 1; n < N; ++n) {
     size_t last_row_last_column = current_row[0];
     current_row[0] = n;
-    for (size_t m = 1; m < M; m++) {
+    for (size_t m = 1; m < M; ++m) {
       if (first[n] != second[m]) {
         // store the desired value of current_row[m] in last_row_last_column
         if (current_row[m - 1] < last_row_last_column)
           last_row_last_column = current_row[m - 1];
         if (current_row[m] < last_row_last_column)
           last_row_last_column = current_row[m];
-        last_row_last_column++;
+        ++last_row_last_column;
       }
       std::swap(last_row_last_column, current_row[m]);
     }
