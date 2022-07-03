56db59b63161e5854c14cd2ce901c7e0ea7a1eed
Amaar Quadri <amaarquadri@gmail.com>
2022-07-02 18:12:29 -0600

    declare a single general algorithm for computing optimal moves

--- include/solvers/SolverUtils.h
+++ include/solvers/SolverUtils.h
@@ -1,9 +1,16 @@
 #pragma once
 
 #include "Face.h"
+#include "StaticVector.h"
+#include "Turn.h"
+#include <algorithm>
 #include <array>
+#include <cassert>
+#include <concepts>
+#include <cstddef>
 #include <cstdint>
 #include <stdexcept>
+#include <utility>
 
 namespace solvers {
 constexpr std::array<uint8_t, 4> getEdgeCycle(const Face& face) {
@@ -50,5 +57,46 @@ constexpr std::array<uint8_t, 4> getCornerCycle(const Face& face) {
     default:
       throw std::logic_error("Unknown enum value!");
   }
+}
+
+template <std::unsigned_integral Uint, Uint DescriptorCount, size_t n>
+constexpr std::array<std::pair<Turn, Uint>, DescriptorCount> getOptimalMoves(
+    const std::array<Turn, n>& possible_turns,
+    Uint (*apply_turn)(const Uint&, const Turn&),
+    const Uint& solved_descriptor = 0) {
+  constexpr Uint UnknownSentinel = DescriptorCount;
+
+  std::array<std::pair<Turn, Uint>, DescriptorCount> optimal_moves{};
+  for (size_t i = 0; i < DescriptorCount; ++i)
+    optimal_moves[i].second = UnknownSentinel;
+
+  utility::StaticVector<Uint, DescriptorCount> current{};
+  current.push_back(solved_descriptor);
+  utility::StaticVector<Uint, DescriptorCount> next{};
+
+  while (!current.isEmpty()) {
+    for (const Uint& idx : current) {
+      for (const Turn& turn : possible_turns) {
+        const Uint next_idx = apply_turn(idx, turn);
+        if (next_idx != solved_descriptor &&
+            optimal_moves[next_idx].second == UnknownSentinel) {
+          optimal_moves[next_idx].first = turn.inv();
+          optimal_moves[next_idx].second = idx;
+          next.push_back(next_idx);
+        }
+      }
+    }
+    current = next;
+    next.clear();
+  }
+  assert(optimal_moves[solved_descriptor].second == UnknownSentinel);
+  assert([&]() {
+    for (size_t i = 0; i < optimal_moves.size(); ++i) {
+      if (i == solved_descriptor) continue;
+      if (optimal_moves[i].second == UnknownSentinel) return false;
+    }
+    return true;
+  }());
+  return optimal_moves;
 }
 }  // namespace solvers
