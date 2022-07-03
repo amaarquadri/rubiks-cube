ac38b75cca82c396ed91253996e7001c2509ccb5
Amaar Quadri <amaarquadri@gmail.com>
2022-07-02 18:08:32 -0600

    using enum Face

--- include/solvers/SolverUtils.h
+++ include/solvers/SolverUtils.h
@@ -7,18 +7,19 @@
 
 namespace solvers {
 constexpr std::array<uint8_t, 4> getEdgeCycle(const Face& face) {
+  using enum Face;
   switch (face) {
-    case Face::U:
+    case U:
       return {0, 1, 2, 3};
-    case Face::F:
+    case F:
       return {2, 4, 8, 7};
-    case Face::R:
+    case R:
       return {1, 5, 9, 4};
-    case Face::B:
+    case B:
       return {0, 6, 10, 5};
-    case Face::L:
+    case L:
       return {3, 7, 11, 6};
-    case Face::D:
+    case D:
       return {8, 9, 10, 11};
     default:
       throw std::logic_error("Unknown enum value!");
