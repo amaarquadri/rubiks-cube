a3a5b29d96924302ab161291d32af283626552ef
Amaar Quadri <amaarquadri@gmail.com>
2022-07-02 18:09:43 -0600

    define getCornerCycle

--- include/solvers/SolverUtils.h
+++ include/solvers/SolverUtils.h
@@ -25,4 +25,30 @@ constexpr std::array<uint8_t, 4> getEdgeCycle(const Face& face) {
       throw std::logic_error("Unknown enum value!");
   }
 }
+
+/**
+ * For the F, R, B, and L faces, the corners will also need to be rotated prior
+ * to being cycled if the RotationAmount is Clockwise or Counterclockwise. In
+ * this case, index 0 and 2 in the resulting array will be rotated Clockwise,
+ * and index 1 and 3 will be rotated Counterclockwise.
+ */
+constexpr std::array<uint8_t, 4> getCornerCycle(const Face& face) {
+  using enum Face;
+  switch (face) {
+    case U:
+      return {0, 1, 2, 3};
+    case F:
+      return {3, 2, 5, 4};
+    case R:
+      return {2, 1, 6, 5};
+    case B:
+      return {1, 0, 7, 6};
+    case L:
+      return {0, 3, 4, 7};
+    case D:
+      return {4, 5, 6, 7};
+    default:
+      throw std::logic_error("Unknown enum value!");
+  }
+}
 }  // namespace solvers
