e41632e154dc6a85e5615496aee3c332357757a1
Amaar Quadri <amaarquadri@gmail.com>
2022-07-07 23:26:55 -0600

    define requiredBits

--- include/utility/MathUtils.h
+++ include/utility/MathUtils.h
@@ -1,7 +1,10 @@
 #pragma once
 
 #include <array>
+#include <bit>
+#include <concepts>
 #include <cstddef>
+#include <cstdint>
 #include <type_traits>
 
 namespace utility {
@@ -39,6 +42,24 @@ constexpr std::common_type_t<T1, T2> nChooseK(const T1& n, const T2& k) {
   }
   return result;
 }
+
+/**
+ * Computes the number of required bits to store the specified number of
+ * distinct values in an unsigned integer.
+ */
+template <std::unsigned_integral T>
+constexpr uint8_t requiredBits(T value) {
+  --value;
+  value |= value >> 1;
+  value |= value >> 2;
+  value |= value >> 4;
+  value |= value >> 8;
+  value |= value >> 16;
+  if constexpr (8 * sizeof(T) > 32) value |= value >> 32;
+  return std::popcount(value);
+}
+static_assert(requiredBits(32ull) == 5);
+static_assert(requiredBits(45ull) == 6);
 
 // Test cases
 static_assert(factorial(5) == 120);
