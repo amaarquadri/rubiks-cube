905886cfe4b0411dd49c6573ca9e38911f1299d2
Amaar Quadri <amaarquadri@gmail.com>
2022-07-13 17:16:49 -0600

    empirically prove properties of the ranks of even and odd permutations

--- src/tests/utility/TestPermutation.cpp
+++ src/tests/utility/TestPermutation.cpp
@@ -25,6 +25,52 @@ static constexpr bool testConsistencyRecursive() {
     return testRankConsistency<n>() && testConsistencyRecursive<n - 1>();
 }
 
+template <size_t n>
+static constexpr auto getParities() {
+  std::array<bool, utility::factorial(n)> parities;
+  for (size_t i = 0; i < parities.size(); ++i)
+    parities[i] = Permutation<n>::parseRank(i).isOdd();
+  return parities;
+}
+
+template <size_t n>
+static constexpr auto getParitiesRecursive() {
+  if constexpr (n == 2)
+    return std::array<bool, 2>{false, true};
+  else {
+    const auto last = getParitiesRecursive<n - 1>();
+    std::array<bool, n * last.size()> result;
+
+    /**
+     * The result is n sequential copies of last, where the odd indexed copies
+     * are inverted.
+     * Thus by induction, for any value of n the result will consist of a
+     * sequence of (false, true) and (true, false) in some order.
+     */
+    for (size_t i = 0; i < n; ++i)
+      for (size_t j = 0; j < last.size(); ++j)
+        result[last.size() * i + j] = (i % 2 == 0) == last[j];
+    return result;
+  }
+}
+
+template <size_t n>
+static constexpr bool testParitiesRecursive() {
+  /**
+   * Since this function returns true for all tested values of n, we have an
+   * empirical proof that getParities and getParitiesRecursive are equivalent
+   * functions.
+   * Thus, the k-th even permutation is either the 2k-th or 2k+1-th
+   * permutation and the k-th odd permutation is the other one. Determining
+   * which one is which is not trivial.
+   */
+  if constexpr (n == 2)
+    return getParities<2>() == getParitiesRecursive<2>();
+  else
+    return getParities<n>() == getParitiesRecursive<n>() &&
+           testParitiesRecursive<n - 1>();
+}
+
 void testPermutation() {
   // test getRank
   static_assert(Permutation<2>{0, 1}.getRank() == 0);
@@ -38,6 +84,7 @@ void testPermutation() {
   static_assert(Permutation<4>{3, 2, 1, 0}.getRank() == 23);
 
   static_assert(testConsistencyRecursive<6>());
+  static_assert(testParitiesRecursive<6>());
 
   std::cout << "Passed all tests for Permutation!\n";
 }
