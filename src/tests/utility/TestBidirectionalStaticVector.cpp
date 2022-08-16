1e6aaad4c6bc6d015302671626f6eb9c20d4a493
Amaar Quadri <amaarquadri@gmail.com>
2022-08-16 02:17:41 -0400

    update BidirectionalStaticVector to use ConstexprSharedPtr for correctness

--- src/tests/utility/TestBidirectionalStaticVector.cpp
+++ src/tests/utility/TestBidirectionalStaticVector.cpp
@@ -1,14 +1,13 @@
 #include "TestBidirectionalStaticVector.h"
 #include "BidirectionalStaticVector.h"
 #include <cstddef>
-#include <stdexcept>
 #include <iostream>
+#include <stdexcept>
 
 void testBidirectionalStaticVector() {
   using namespace utility;
 
-  auto [data_store, forward, backward] =
-      BidirectionalStaticVector<int, 100>::make();
+  auto [forward, backward] = BidirectionalStaticVector<int, 100>::make();
   for (size_t i = 0; i < 40; ++i) forward.push_back(5);
   for (size_t i = 0; i < 59; ++i) backward.push_back(6);
 
