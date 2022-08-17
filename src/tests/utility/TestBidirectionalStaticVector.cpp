f671325905b1e246f8972659979b296492fc60da
Amaar Quadri <amaarquadri@gmail.com>
2022-08-17 05:26:28 -0400

    modify TestBidirectionalStaticVector.cpp to test for both use_heap=false and use_heap=true

--- src/tests/utility/TestBidirectionalStaticVector.cpp
+++ src/tests/utility/TestBidirectionalStaticVector.cpp
@@ -4,16 +4,22 @@
 #include <iostream>
 #include <stdexcept>
 
-void testBidirectionalStaticVector() {
+template <bool use_heap>
+static void testForUseHeap() {
   using namespace utility;
-
-  auto [forward, backward] = BidirectionalStaticVector<int, 100>::make();
+  auto [forward, backward] =
+      BidirectionalStaticVector<int, 100, use_heap>::make();
   for (size_t i = 0; i < 40; ++i) forward.push_back(5);
   for (size_t i = 0; i < 59; ++i) backward.push_back(6);
 
   if (forward.getSize() != 40 || backward.getSize() != 59 ||
       forward.isEmpty() || backward.isEmpty())
     throw std::logic_error("Incorrect sizes!");
+}
+
+void testBidirectionalStaticVector() {
+  testForUseHeap<false>();
+  testForUseHeap<true>();
 
   std::cout << "Passed all tests for BidirectionalStaticVector!\n";
 }
