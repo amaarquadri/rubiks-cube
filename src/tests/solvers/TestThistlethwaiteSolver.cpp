831e194f5ab112c575f95c8335cff38cadb44cad
Amaar Quadri <amaarquadri@gmail.com>
2022-08-18 18:58:13 -0400

    added TestThistlethwaiteSolver

---
+++ src/tests/solvers/TestThistlethwaiteSolver.cpp
@@ -0,0 +1,27 @@
+#include "TestThistlethwaiteSolver.h"
+#include "Algorithm.h"
+#include "Cube.h"
+#include "ThistlethwaiteSolver.h"
+#include <iostream>
+#include <stdexcept>
+
+using namespace solvers;
+
+static void testSolveThistlethwaite() {
+  static constexpr size_t Count = 1000;
+
+  for (size_t i = 0; i < Count; ++i) {
+    const Algorithm scramble = Algorithm::random(20);
+    Cube cube{scramble};
+    const Algorithm solve = solveThistlethwaite(cube);
+    cube.apply(solve);
+    if (!cube.isSolved()) throw std::logic_error("Cube was not solved!");
+  }
+}
+
+void testThistlethwaiteSolver() {
+  runThistlethwaiteSolverTests();
+  testSolveThistlethwaite();
+
+  std::cout << "Passed all tests for ThistlethwaiteSolver!\n";
+}
