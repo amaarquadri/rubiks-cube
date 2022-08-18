3635d9832699323e9c83e039d412145023610306
Amaar Quadri <amaarquadri@gmail.com>
2022-08-18 19:15:53 -0400

    revert to static for RandomUtils.h to prevent linking errors

--- include/utility/RandomUtils.h
+++ include/utility/RandomUtils.h
@@ -6,24 +6,24 @@
 #include <random>
 
 namespace utility {
-const unsigned seed =  // NOLINT(cert-err58-cpp)
+static const unsigned seed =  // NOLINT(cert-err58-cpp)
     std::chrono::system_clock::now().time_since_epoch().count();
-auto random_engine =  // NOLINT(cert-err58-cpp)
+static auto random_engine =  // NOLINT(cert-err58-cpp)
     std::default_random_engine(seed);
 
 /**
  * @return A random integer in [0, n).
  */
 template <size_t n>
-size_t randomInt() {
+static size_t randomInt() {
   static std::uniform_int_distribution<size_t> distribution(0, n - 1);
   return distribution(random_engine);
 }
 
-bool randomBool() { return randomInt<1>(); }
+static bool randomBool() { return randomInt<1>(); }
 
 template <typename T, size_t n>
-T pickRandom(const std::array<T, n>& values) {
+static T pickRandom(const std::array<T, n>& values) {
   return values[randomInt<n>()];
 }
 }  // namespace utility
