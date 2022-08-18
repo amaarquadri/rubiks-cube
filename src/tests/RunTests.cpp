dc9de1e92c7d1f40aee205698cc2994fe37f36ca
Amaar Quadri <amaarquadri@gmail.com>
2022-08-18 19:19:03 -0400

    add testHalfTurnReductionSolver and testThistlethwaiteSolver to CMakeLists.txt and RunTests.cpp (commented out)

--- src/tests/RunTests.cpp
+++ src/tests/RunTests.cpp
@@ -8,10 +8,12 @@
 #include "TestDominoReductionSolver.h"
 #include "TestEdgeCycleSequenceIterator.h"
 #include "TestEdgeOrientationSolver.h"
+#include "TestHalfTurnReductionSolver.h"
 #include "TestPLLs.h"
 #include "TestPackedBitsArray.h"
 #include "TestPermutation.h"
 #include "TestReconstructionIterator.h"
+#include "TestThistlethwaiteSolver.h"
 #include <cstddef>
 #include <iostream>
 
@@ -28,6 +30,8 @@ void runTests() {
   testEdgeCycleSequenceIterator();
   testDominoReductionSolver();
   testEdgeOrientationSolver();
+  //  testHalfTurnReductionSolver();
+  //  testThistlethwaiteSolver();
 
   static constexpr size_t Count = 10;
   for (size_t i = 0; i < Count; ++i) {
