b7f0f665fe9ee8e94acf3f18046d7c198acac440
Amaar Quadri <amaarquadri@gmail.com>
2022-08-18 17:58:47 -0400

    remove static specifiers from RandomUtils.h since they should not be compilation unit specific

--- include/utility/RandomUtils.h
+++ include/utility/RandomUtils.h
@@ -5,19 +5,19 @@
 #include <random>
 
 namespace utility {
-static const unsigned seed =  // NOLINT(cert-err58-cpp)
+const unsigned seed =  // NOLINT(cert-err58-cpp)
     std::chrono::system_clock::now().time_since_epoch().count();
-static auto random_engine =  // NOLINT(cert-err58-cpp)
+auto random_engine =  // NOLINT(cert-err58-cpp)
     std::default_random_engine(seed);
 
 /**
  * @return A random integer in [0, n).
  */
 template <size_t n>
-static size_t randomInt() {
+size_t randomInt() {
   static std::uniform_int_distribution<size_t> distribution(0, n - 1);
   return distribution(random_engine);
 }
 
-static bool randomBool() { return randomInt<1>(); }
+bool randomBool() { return randomInt<1>(); }
 }  // namespace utility
