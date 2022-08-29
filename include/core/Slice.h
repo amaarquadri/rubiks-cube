7eeb07bacaf5fee2a5e2fcb8d56d89fc26430131
Amaar Quadri <amaarquadri@gmail.com>
2022-08-29 01:01:32 -0400

    make some functions in Slice.h constexpr, code cleanup

--- include/core/Slice.h
+++ include/core/Slice.h
@@ -4,6 +4,7 @@
 #include "RotationAxis.h"
 #include <cstddef>
 #include <cstdint>
+#include <stdexcept>
 #include <string>
 #include <utility>
 
@@ -13,22 +14,61 @@ enum class Slice : uint8_t { M, E, S };
  * @return The face that has the same rotation axis and direction as the given
  * Slice
  */
-Face getRotationFace(const Slice& slice);
+constexpr Face getRotationFace(const Slice& slice) {
+  switch (slice) {
+    case Slice::M:
+      return Face::L;
+    case Slice::E:
+      return Face::D;
+    case Slice::S:
+      return Face::F;
+    default:
+      throw std::logic_error("Unknown enum value!");
+  }
+}
 
 /**
  * @brief Computes the RotationAxis that results from applying this Slice with a
  * RotationAmount of Clockwise. If the bool is true, then the RotationAxis is in
  * the opposite direction
  */
-std::pair<RotationAxis, bool> getRotationAxis(const Slice& slice);
+constexpr std::pair<RotationAxis, bool> getRotationAxis(const Slice& slice) {
+  switch (slice) {
+    case Slice::M:
+      return {RotationAxis::X, true};
+    case Slice::E:
+      return {RotationAxis::Y, true};
+    case Slice::S:
+      return {RotationAxis::Z, false};
+    default:
+      throw std::logic_error("Unknown enum value!");
+  }
+}
 
 /**
  * @breif Computes the slice that has the same rotation direction as the given
  * Face. If the bool is true, then the slice is the opposite direction
  */
-std::pair<Slice, bool> fromRotationFace(const Face& face);
+constexpr std::pair<Slice, bool> fromRotationFace(const Face& face) {
+  switch (face) {
+    case Face::L:
+      return {Slice::M, false};
+    case Face::R:
+      return {Slice::M, true};
+    case Face::D:
+      return {Slice::E, false};
+    case Face::U:
+      return {Slice::E, true};
+    case Face::F:
+      return {Slice::S, false};
+    case Face::B:
+      return {Slice::S, true};
+    default:
+      throw std::logic_error("Unknown enum value!");
+  }
+}
 
-std::string toStr(const Slice& face);
+std::string toStr(const Slice& slice);
 
 /**
  * @brief Returns the number of characters consumed and the parsed Slice. If it
--- src/core/Slice.cpp
+++ src/core/Slice.cpp
@@ -1,63 +1,18 @@
 #include "Slice.h"
-#include "Face.h"
-#include "RotationAxis.h"
 #include <cstddef>
 #include <stdexcept>
 #include <string>
 #include <utility>
 
-Face getRotationFace(const Slice& slice) {
+using enum Slice;
+
+std::string toStr(const Slice& slice) {
   switch (slice) {
-    case Slice::M:
-      return Face::L;
-    case Slice::E:
-      return Face::D;
-    case Slice::S:
-      return Face::F;
-    default:
-      throw std::logic_error("Unknown enum value!");
-  }
-}
-
-std::pair<RotationAxis, bool> getRotationAxis(const Slice& slice) {
-  switch (slice) {
-    case Slice::M:
-      return {RotationAxis::X, true};
-    case Slice::E:
-      return {RotationAxis::Y, true};
-    case Slice::S:
-      return {RotationAxis::Z, false};
-    default:
-      throw std::logic_error("Unknown enum value!");
-  }
-}
-
-std::pair<Slice, bool> fromRotationFace(const Face& face) {
-  switch (face) {
-    case Face::L:
-      return {Slice::M, false};
-    case Face::R:
-      return {Slice::M, true};
-    case Face::D:
-      return {Slice::E, false};
-    case Face::U:
-      return {Slice::E, true};
-    case Face::F:
-      return {Slice::S, false};
-    case Face::B:
-      return {Slice::S, true};
-    default:
-      throw std::logic_error("Unknown enum value!");
-  }
-}
-
-std::string toStr(const Slice& face) {
-  switch (face) {
-    case Slice::M:
+    case M:
       return "M";
-    case Slice::E:
+    case E:
       return "E";
-    case Slice::S:
+    case S:
       return "S";
     default:
       throw std::logic_error("Unknown enum value!");
@@ -66,13 +21,14 @@ std::string toStr(const Slice& face) {
 
 std::pair<size_t, Slice> parseSlice(const std::string& str) {
   if (str.empty()) return {0, {}};
-  const char& first_char = str[0];
-  if (first_char == 'M')
-    return {1, Slice::M};
-  else if (first_char == 'E')
-    return {1, Slice::E};
-  else if (first_char == 'S')
-    return {1, Slice::S};
-  else
-    return {0, {}};
+  switch (str[0]) {
+    case 'M':
+      return {1, M};
+    case 'E':
+      return {1, E};
+    case 'S':
+      return {1, S};
+    default:
+      return {0, {}};
+  }
 }
